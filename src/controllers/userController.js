b53e59bd10bf03b27a1b0fcf3b65b0246bdfdc9f
isanket87 <isanket787@gmail.com>
2025-08-31 22:43:01 +0530

    change role

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -100,7 +100,7 @@ exports.getAllUsers = async (req, res) => {
     try {
         let baseQuery = 'FROM users';
         // Exclude SUPER_ADMIN from the list of users that can be managed here.
-        let whereClause = " WHERE role != 'SUPER_ADMIN'";
+        let whereClause = " WHERE role != 'SUPERADMIN'";
         let queryParams = [];
         let paramIndex = 1;
 
